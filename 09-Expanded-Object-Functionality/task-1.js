a8c49b4d8ceba6d5d47b3754ffca6217afb4b487
khvedelidz3 <erekle.khvedelidze08@geolab.edu.ge>
2019-06-05 12:30:38 +0400

    feat(09-Expanded-Object-Functionality): Finish task-1

--- 09-Expanded-Object-Functionality/task-1.js
+++ 09-Expanded-Object-Functionality/task-1.js
@@ -1,26 +1,24 @@
 Object.defineProperty(Object.prototype, 'extend', {
 
     value(source) {
-        for (val in Object.getOwnPropertyDescriptors(source)) {
-            Object.defineProperty(this, val, {
-                value: Object.getOwnPropertyDescriptors(source)[val].value, 
-                writable: Object.getOwnPropertyDescriptors(source)[val].writable,
-                enumerable: Object.getOwnPropertyDescriptors(source)[val].enumerable,
-                configurable: Object.getOwnPropertyDescriptors(source)[val].configurable
+        let descriptors = Object.getOwnPropertyDescriptors(source);
+
+        Object.keys(descriptors).forEach((key) => {
+            Object.defineProperty(this, key, {
+                value: this.hasOwnProperty(key) ? this[key] : descriptors[key].value,
+                writable: descriptors[key].writable,
+                enumerable: descriptors[key].enumerable,
+                configurable: descriptors[key].configurable
             })
-        }
+        });
     }
-})
+});
 
-const data = { a: 'a' };
-const source = { a: 'A', b: 'b' };
+const data = {a: 'a'};
+const source = {a: 'A', b: 'b'};
 
-Object.defineProperty(source, 'b', { writable: false });
+Object.defineProperty(source, 'b', {writable: false});
 
 data.extend(source);
 
 console.log(Object.getOwnPropertyDescriptors(data))
-
-
-
-// console.log(Object.getOwnPropertyDescriptors(source))
