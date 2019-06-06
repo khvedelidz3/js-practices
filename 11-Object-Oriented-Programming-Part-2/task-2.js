fb786b4909a24216497a7e1777b93c079ff8fdb0
khvedelidz3 <erekle.khvedelidze08@geolab.edu.ge>
2019-06-07 00:02:57 +0400

    feat(11-Object-Oriented-Programming-Part-2)

--- 11-Object-Oriented-Programming-Part-2/task-2.js
+++ 11-Object-Oriented-Programming-Part-2/task-2.js
@@ -1,33 +1,21 @@
 CoffeeMachine.prototype.getTimeToBoil = function() {
-    return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
-}
+    return this.waterAmount * this.WATER_HEAT_CAPACITY * 80 / this.power;
+};
 
 CoffeeMachine.prototype.run = function() {
     setTimeout(function() {
         console.log('Coffee is ready!');
-    }, getTimeToBoil());
-}
+    }, this.getTimeToBoil());
+};
 
 CoffeeMachine.prototype.setWaterAmount = function(amount) {
-    waterAmount = amount;
-}
+    this.waterAmount = amount;
+};
 
 function CoffeeMachine(power) {
-	let waterAmount = 0;
-    const WATER_HEAT_CAPACITY = 4200;
-	
-    // function getTimeToBoil() {
-	// 	return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
-	// }
-  	
-    // this.run = function() {
-    // 	setTimeout(function() {
-	// 		console.log('Coffee is ready!');
-    // 	}, getTimeToBoil());
-	// };
-	// this.setWaterAmount = function(amount) {
-	// 	waterAmount = amount;
-	// };
+    this.waterAmount = 0;
+    this.WATER_HEAT_CAPACITY = 4200;
+    this.power = power;
 }
 
 let coffeeMachine = new CoffeeMachine(10000);
