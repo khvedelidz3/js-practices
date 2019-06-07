d8945e7cc3325cfa27932044665e2864c6d1886e
khvedelidz3 <erekle.khvedelidze08@geolab.edu.ge>
2019-06-07 10:52:28 +0400

    feat(11-Object-Oriented-Programming-Part-2): Finish task-3

---
+++ 11-Object-Oriented-Programming-Part-2/task-3.js
@@ -0,0 +1,32 @@
+function Hamster() {
+    Hamster.prototype._food = [];
+
+    Hamster.prototype._found = function(something) {
+        this.food.push(something);
+    };
+
+    return {
+        food: this._food,
+        found: this._found
+    }
+}
+
+// Hamster.prototype.food = []; // empty "stomach"
+
+// Hamster.prototype.found = function(something) {
+//     this.food.push(something);
+// };
+
+// Create two hamsters and feed the first
+let speedy = new Hamster();
+let lazy = new Hamster();
+
+speedy.found("apple");
+speedy.found("nut");
+
+lazy.found('test1');
+lazy.found('test2');
+lazy.found('test3');
+
+console.log(speedy.food.length); // 2
+console.log(lazy.food.length); // 2 (!??)
