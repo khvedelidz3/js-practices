617f22d9dc3f15a2ac5400b4f2820f89d4cfe81b
khvedelidz3 <erekle.khvedelidze08@geolab.edu.ge>
2019-06-14 12:18:17 +0400

    feat(01-Promises): Finish task-3 and task-4

--- 01-Promises/task-3.js
+++ 01-Promises/task-3.js
@@ -1,11 +1,18 @@
 function send(url) {
-
+    return new Promise((resolve, reject) => {
+        get(url, (error, meta, body) => {
+            if(meta.status === 200) {
+                const { data } = JSON.parse(body);
+                resolve(data);
+            } else {
+                reject(`We have error, status code: ${meta.status}`)
+            }
+        });
+    })
 }
 
 const get = require('fetch').fetchUrl;
 const url = 'https://lab.lectrum.io/geo/api/countries?size=2';
-
-console.log()
 
 send(url)
     .then(data => {
---
+++ 01-Promises/task-4.js
@@ -0,0 +1,37 @@
+class Countries {
+    constructor(val) {
+        if(typeof val !== 'string') {
+            throw new Error('Url parameter should be string');
+        }
+
+        this.url = val;
+    }
+
+    send(size) {
+        return new Promise((resolve, reject) => {
+            get(this.url+'?size='+size, (error, meta, body) => {
+                if (meta.status === 200) {
+                    const {data} = JSON.parse(body);
+                    resolve(data);
+                } else {
+                    reject(`We have error, status code: ${meta.status}`)
+                }
+            });
+        });
+    }
+
+}
+
+const get = require('fetch').fetchUrl;
+
+const url = 'https://lab.lectrum.io/geo/api/countries';
+const countries = new Countries(url);
+
+(async () => {
+    try {
+        const data = await countries.send(2);
+        console.log(data); // array of countries
+    } catch (error) {
+        console.log(error);
+    }
+})();
