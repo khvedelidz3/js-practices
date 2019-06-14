21a72194c54a33e6eb1a67e08c563b6dc348a284
khvedelidz3 <erekle.khvedelidze08@geolab.edu.ge>
2019-06-14 21:10:48 +0400

    feat(18-Iterators-and-Generators): Finish all tasks

---
+++ 18-Iterators-and-Generators/task-1.js
@@ -0,0 +1,54 @@
+class Customers {
+    constructor() {
+        this.customers = [];
+        // this.i = 0;
+        // const self = this;
+    }
+
+    [Symbol.iterator]() {
+        let customers = this.customers.filter(item => {
+            return item.verified;
+        });
+        let i = 0;
+        return {
+            next() {
+                const done = i >= customers.length;
+                const value = !done ? customers[i++] : undefined;
+
+                return {
+                    value,
+                    done
+                }
+            }
+        }
+    }
+
+
+
+    add(item) {
+        this.customers.push(item);
+    }
+}
+
+const customers = new Customers();
+customers.add({ name: 'Alex' });
+customers.add({ name: 'Victor' });
+customers.add({ name: 'Marcus' });
+customers.add({ name: 'Andrii', verified: true });
+customers.add({ name: 'Marco', verified: true });
+customers.add({ name: 'Oliver' });
+customers.add({ name: 'Lisa', verified: true });
+customers.add({ name: 'John' });
+customers.add({ name: 'Ivan', verified: true });
+
+// console.log(customers.customers)
+
+for (const customer of customers) {
+    console.log(customer);
+}
+
+// console log
+// { name: 'Andrii', verified: true }
+// { name: 'Marco', verified: true }
+// { name: 'Lisa', verified: true }
+// { name: 'Ivan', verified: true }
